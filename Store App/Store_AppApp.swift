94d224497c6d2c3905ef588f678e6cd86ecab776
Egor <korchagin.egorka1@gmail.com>
2021-03-28 11:08:47 +1000

    Initial Commit

---
+++ Store App/Store_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Store_AppApp.swift
+//  Store App
+//
+//  Created by Egor Korchagin on 28.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Store_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
